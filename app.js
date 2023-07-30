1426b76dba4b68ea93e2f223d09ccc8d49692b70
Carlos CaravanTsz <carloscara28@hotmail.com>
2023-07-29 21:51:53 -0600

    Volviendo al puerto 8080

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import viewsRouter from './src/routes/views.router.js'
 import ProductManager from './src/manager/product.manager.js'
 
 // SERVER INIT CONFIG
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // MIDDLEWARES CONFIG
